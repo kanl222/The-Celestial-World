0be8dacd1aad0bafcffc824a0d372a82cd83d125
Kanl222 <kanl222fdd@gmailcom>
2022-11-24 01:35:36 +0500

    Base commit

--- 32.py
+++
@@ -1,3 +0,0 @@
-import json
-
-with open("Json/Magic.json", "r") as read_file:
--- 3443.py
+++
@@ -1,4 +0,0 @@
-count = int(input())
-duration = int(input())
-change = int(input())
-print(count * duration + ((count - 2) * change) + 30)
